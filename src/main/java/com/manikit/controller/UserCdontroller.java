89eba5a94d856b65e8b1252eb2263494f85c5d9f
manikdahiphale2016 <manik.dahiphale2016@gmail.com>
2022-01-03 13:49:22 +0530

    getUser() method added

--- src/main/java/com/manikit/controller/UserCdontroller.java
+++ src/main/java/com/manikit/controller/UserCdontroller.java
@@ -1,5 +1,14 @@
 package com.manikit.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserCdontroller {
+	
+	@GetMapping
+	public String getUser() {
+		return "";
+	}
 
 }
