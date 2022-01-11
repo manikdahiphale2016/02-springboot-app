ab8f3231701756bea41af846ffe6d84ca247622d
manikdahiphale2016 <manik.dahiphale2016@gmail.com>
2022-01-11 21:03:35 +0530

    REST_URL is added

--- src/main/java/com/manikit/Application.java
+++ src/main/java/com/manikit/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+	private String REST_URL = "https://www.google.com";
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
