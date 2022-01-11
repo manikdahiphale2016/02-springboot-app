c0fb81ab27684e55f2e2262f8c3ff08cec437dcf
Manik Dahiphale <52069604+manikdahiphale2016@users.noreply.github.com>
2022-01-11 21:12:31 +0530

    Update Application.java

--- src/main/java/com/manikit/Application.java
+++ src/main/java/com/manikit/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-		private static final Logger logger = Logger.getLogger(Application.class);
+	private static final Logger logger = Logger.getLogger(Application.class);
 	private String REST_URL = "https://www.google.com";
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
