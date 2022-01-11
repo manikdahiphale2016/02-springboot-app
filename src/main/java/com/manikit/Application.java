aebfc960ab31373f56a50dd3002801b167d40866
manikdahiphale2016 <manik.dahiphale2016@gmail.com>
2022-01-11 21:07:57 +0530

    Logger added

--- src/main/java/com/manikit/Application.java
+++ src/main/java/com/manikit/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+		private static final Logger logger = Logger.getLogger(Application.class);
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
